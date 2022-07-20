7d4605f8c9af517bb7828f891c18a367c8c68400
Zeitgeist <shecksk8nick@gmail.com>
2022-07-19 20:59:22 -0500

    Fixed percentages

--- src/views/results/results.tsx
+++ src/views/results/results.tsx
@@ -12,7 +12,7 @@ export const Results: React.FC = () => {
         <div className='result' key={i}>
           <img src={result.imageUrl} alt={result.fullName} />
           <div className='data-container'>
-            <h2 className='percentil'>{result.total / totalVotes}%</h2>
+            <h2 className='percentil'>{(result.total / totalVotes * 100).toFixed(2)}%</h2>
             <h3 className='votes'>({result.total})</h3>
           </div>
         </div>
